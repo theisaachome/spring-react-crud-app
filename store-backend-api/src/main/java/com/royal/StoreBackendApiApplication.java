19c683b9aa0de481a25517ce9b5ad2e2022be9d7
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-06-05 06:02:28 +0630

    setup project for backend api

---
+++ store-backend-api/src/main/java/com/royal/StoreBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.royal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreBackendApiApplication.class, args);
+	}
+
+}
---
+++ store-backend-api/src/test/java/com/royal/StoreBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.royal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
