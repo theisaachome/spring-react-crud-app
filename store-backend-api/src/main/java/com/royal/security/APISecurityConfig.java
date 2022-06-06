8af5243276e08517c3b76afe0c3c1171d715972b
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-06-06 15:46:16 +0630

    Add endpoint and jwt filter setup

--- store-backend-api/src/main/java/com/royal/security/APISecurityConfig.java
+++ store-backend-api/src/main/java/com/royal/security/APISecurityConfig.java
@@ -3,41 +3,71 @@ package com.royal.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.royal.security.jwt.JwtAuthenticationEntryPoint;
+import com.royal.security.jwt.JwtAuthenticationFilter;
+
 /**
  *
- *@author Isaachome
+ * @author Isaachome
  */
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)// method level security
+@EnableGlobalMethodSecurity(prePostEnabled = true) // method level security
 public class APISecurityConfig {
-	
+
 	@Autowired
 	private CustomUserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtAuthenticationEntryPoint authenticationEntryPoint;
+
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	
-	 @Bean
-	 protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		 http
-		 	.csrf().disable()
-		 	.authorizeHttpRequests((authorize)->{
-		 		authorize.antMatchers("/api/auth/**").permitAll()
-		 		.anyRequest()
-		 		.authenticated();
-		 	});
-		 return http.build();
-	 }
 
-	
+	@Bean
+	protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+		.csrf().disable()
+		.exceptionHandling()
+		.authenticationEntryPoint(authenticationEntryPoint)
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authorizeHttpRequests((authorize) -> {
+			authorize.antMatchers("/api/auth/**").permitAll()
+			.antMatchers("/api/auth/**").permitAll()
+            .antMatchers("/v2/api-docs/**").permitAll()
+            .antMatchers("/swagger-ui/**").permitAll()
+            .antMatchers("/swagger-resources/**").permitAll()
+            .antMatchers("/swagger-ui.html").permitAll()
+            .antMatchers("/webjars/**").permitAll()
+			.anyRequest().authenticated();
+		});
+		return http.build();
+	}
+
 }
